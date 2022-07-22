81ca8be2249875353cc3d70eb14549c5984972f4
RenaudGrct <grct.renaud@gmail.com>
2022-07-22 23:26:26 +0200

    :bug: fix : merge conflicts

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -2,7 +2,6 @@ const ApiError = require("../errors/apiError.js");
 const userDatamapper = require("../models/user.js");
 
 
-<<<<<<< HEAD
 module.exports = {
   /**
     * User controller pour obtenir une entrée.
@@ -19,56 +18,6 @@ module.exports = {
     }
     return res.json(result);
   },
-=======
-  async findUser(req, res) {
-    try {
-      const userId = parseInt(req.params.id);
-      const result = await userDatamapper.findByPk(userId);
-      res.json(result);
-    } catch (error) {
-    // res.status(error.response.status).send("Une erreur est survenue");
-      console.log(error);
-    }
-  },
-
-  async createUser(req, res) {
-    try {
-      const result = await userDatamapper.insert(req.body);
-      res.send(result);
-    } catch (error) {
-      // res.status(error.response.status).send("Une erreur est survenue");
-      console.log(error);
-    }
-
-  },
-
-  async updateUser(req, res) {
-    try {
-
-      const userId = parseInt(req.params.id);
-      const result = await userDatamapper.findByPk(userId);
-      if (!result) {
-        return res.status(404).json("Modification impossible");
-      }
-      await userDatamapper.update(userId, req.body);
-      return res.status(200).json("Modification réussie");
-    } catch (error) {
-      // res.status(error.response.status).send("Une erreur est survenue");
-      console.log(error);
-    }
-  },
-
-  async deleteUser(req, res) {
-    try {
-      const userId = parseInt(req.params.id);
-      await userDatamapper.delete(userId);
-      return res.status(200).json("L'utilisateur a été supprimé");
-    } catch (error) {
-      // res.status(error.response.status).send("Une erreur est survenue");
-      console.log(error.response.status);
-    }
-  }
->>>>>>> dev
 
   /**
     * User controller pour créer une entrée.
