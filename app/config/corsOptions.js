41664c846dbd15f940c220ef07f22f965c2a97c0
RenaudGrct <grct.renaud@gmail.com>
2022-07-27 18:04:28 +0200

    déploiement 27/07/2022 #17

--- app/config/corsOptions.js
+++ app/config/corsOptions.js
@@ -1,6 +1,5 @@
 const corsOptions = {
   origin: process.env.BACKEND_LOCAL_URL,
-  preflightContinue: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionSuccessStatus: 200
