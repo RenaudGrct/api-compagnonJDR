e1daf4e24a6d7b7ec64b6b985589c4023bfd90fe
RenaudGrct <grct.renaud@gmail.com>
2022-07-27 17:12:19 +0200

    déploiement 27/07/2022 #12

--- app/config/corsOptions.js
+++ app/config/corsOptions.js
@@ -6,12 +6,13 @@ const corsOptions = {
     if(!allowedOrigins.includes(origin)){
       throw new ApiError("Origin not allowed by CORS", { statusCode: 503 });
     }
-    return true;
+    return origin;
   },
   preflightContinue: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionSuccessStatus: 200
 };
+console.log(`🚀 ~ corsOptions`, corsOptions);
 
 module.exports = corsOptions;
