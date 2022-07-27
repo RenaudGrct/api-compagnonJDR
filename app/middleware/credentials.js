bcc853bdf84bd9c6f50381d66f6c429e1c8f8c85
RenaudGrct <grct.renaud@gmail.com>
2022-07-27 16:20:10 +0200

    déploiement 27/07/2022 #5

--- app/middleware/credentials.js
+++ app/middleware/credentials.js
@@ -3,6 +3,7 @@ const allowedOrigins = require("../config/allowedOrigins");
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)){
+    res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.setHeader("Acces-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
     res.setHeader("Acces-Control-Allow-Credentials", true);
