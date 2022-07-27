5796aba8bb827118354c9a7d28db5f31c1c81f83
RenaudGrct <grct.renaud@gmail.com>
2022-07-27 17:16:12 +0200

    déploiement 27/07/2022 #13

--- app/config/corsOptions.js
+++ app/config/corsOptions.js
@@ -2,17 +2,11 @@ const ApiError = require("../errors/apiError");
 const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
-  origin: (origin) =>{
-    if(!allowedOrigins.includes(origin)){
-      throw new ApiError("Origin not allowed by CORS", { statusCode: 503 });
-    }
-    return origin;
-  },
+  origin: "http://localhost:8080",
   preflightContinue: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionSuccessStatus: 200
 };
-console.log(`🚀 ~ corsOptions`, corsOptions);
 
 module.exports = corsOptions;
