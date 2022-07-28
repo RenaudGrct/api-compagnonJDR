36264b54202989581222cd7c86075a994e44af43
RenaudGrct <grct.renaud@gmail.com>
2022-07-28 10:38:36 +0200

    accessToken date d'expiration : 1jours  déploiement Heroku 28/07/2022

--- app/services/Token/generateToken.js
+++ app/services/Token/generateToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = {
   async generateAccessToken (user){
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m"});
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d"});
   },
 
   async generateRefreshToken (user){
