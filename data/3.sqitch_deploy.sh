c5a5c733a7d7c8180b850bd9879975c063fbfd30
RenaudGrct <99024135+RenaudGrct@users.noreply.github.com>
2022-07-28 11:12:21 +0200

    Update 3.sqitch_deploy.sh

--- data/3.sqitch_deploy.sh
+++ data/3.sqitch_deploy.sh
@@ -3,7 +3,7 @@ export PGUSER=cjdr
 export PGPASSWORD=cjdr
 
 #* Deploy Global :
-#sqitch deploy -d cjdr 1.userTable
-#sqitch deploy -d cjdr 2.guestTable
-#sqitch deploy -d cjdr 3.refreshToken
-#sqitch deploy -d cjdr 4.characterTables
+#sqitch deploy -d compagnon-jdr 1.userTable
+#sqitch deploy -d compagnon-jdr 2.guestTable
+#sqitch deploy -d compagnon-jdr 3.refreshToken
+#sqitch deploy -d compagnon-jdr 4.characterTables
