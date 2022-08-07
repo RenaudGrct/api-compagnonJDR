80436d5b21458ff13cd2c46c315b37d0b60bb0d5
RenaudGrct <grct.renaud@gmail.com>
2022-08-07 18:13:23 +0200

    déploiement Heroku 07/08/2022 #6

--- app/config/allowedOrigins.js
+++ app/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
   "http://localhost:4000",
   "http://localhost:8080",
-  "https://api-compagnon-jdr.herokuapp.com"
+  "https://api-compagnon-jdr.herokuapp.com",
+  "https://compagnonjdr.netlify.app"
 ];
 
 module.exports = allowedOrigins ;
