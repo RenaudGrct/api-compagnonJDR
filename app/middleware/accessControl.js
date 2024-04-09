2686b4753e0962a0bef470509c057916e5217616
RenaudGrct <Grct.Renaud@gmail.com>
2024-04-09 19:46:56 +0200

    :bug: Ajout d'un retour d'erreur 400 en cas d'origin non autorisées

--- app/middleware/accessControl.js
+++ app/middleware/accessControl.js
@@ -1,16 +1,17 @@
 require("dotenv").config();
 const allowedOrigins = require("../config/allowedOrigins");
+const ApiError = require("../errors/apiError");
 
 module.exports = {
   accessControl (req, res, next){
     const origin = req.headers.origin;
-    console.log(origin);
-    console.log(req.headers);
     if (allowedOrigins.includes(origin)) {
       res.header("Access-Control-Allow-Origin", origin);
       res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
       res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
       res.header("Access-Control-Allow-Credentials", true);
+    } else {
+      throw ApiError(`Aucune origin autorisées : ${origin}`, {statusCode: 400});
     }
     // response to preflight request
     if (req.method === "OPTIONS") {
