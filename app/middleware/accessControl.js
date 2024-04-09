c0b0541942d240de1d3beddde889ffb654ec6d37
RenaudGrct <Grct.Renaud@gmail.com>
2024-04-09 19:50:49 +0200

    :bug: check origin in headers

--- app/middleware/accessControl.js
+++ app/middleware/accessControl.js
@@ -1,18 +1,16 @@
 require("dotenv").config();
 const allowedOrigins = require("../config/allowedOrigins");
-const ApiError = require("../errors/apiError");
 
 module.exports = {
   accessControl (req, res, next){
     const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-      res.header("Access-Control-Allow-Origin", origin);
-      res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
-      res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-      res.header("Access-Control-Allow-Credentials", true);
-    } else {
-      throw ApiError(`Aucune origin autorisées : ${origin}`, {statusCode: 400});
-    }
+    // if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Current-origin", origin);
+    res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Credentials", true);
+    // }
     // response to preflight request
     if (req.method === "OPTIONS") {
       return res.sendStatus(200);
