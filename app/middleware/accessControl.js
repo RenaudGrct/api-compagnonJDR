05da80e952f14112539007b8ffb03e1164e09ffb
RenaudGrct <Grct.Renaud@gmail.com>
2024-04-10 15:22:49 +0200

    :bug: fix CORS issue

--- app/middleware/accessControl.js
+++ app/middleware/accessControl.js
@@ -4,15 +4,16 @@ const allowedOrigins = require("../config/allowedOrigins");
 module.exports = {
   accessControl (req, res, next){
     const origin = req.headers.origin;
-    // if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-    res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    res.header("Access-Control-Allow-Credentials", true);
-    // }
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers");
+      res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE");
+      res.setHeader("Access-Control-Allow-Private-Network", true);
+      res.setHeader("Access-Control-Allow-Credentials", true);
+    }
     // response to preflight request
     if (req.method === "OPTIONS") {
-      return res.sendStatus(200);
+      return res.sendStatus(204);
     }
     next();
   }
